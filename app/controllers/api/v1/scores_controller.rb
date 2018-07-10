3bf47cb2b53ba7289bc1173a18db0e01f38374a6
JohnZBurnett <tuninator@gmail.com>
2018-07-10 13:35:16 -0400

    create topten route and controller action

--- app/controllers/api/v1/scores_controller.rb
+++ app/controllers/api/v1/scores_controller.rb
@@ -12,6 +12,15 @@ class Api::V1::ScoresController < ApplicationController
   def create
     @score = Score.new(point_value: params[:point_value], user_id: params[:user_id])
     @score.save
-    render json: @score 
+    render json: @score
+  end
+
+  def topten
+    @scores = Score.all
+    top_ten_list = @scores.sort_by do |score|
+      score.point_value.to_i
+    end.reverse.first(10)
+
+    render json: top_ten_list
   end
 end
